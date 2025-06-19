8c272f7fa24d30bcb13ccb5279cba6576c244d8e
yampeku <yampeku@gmail.com>
2025-06-20 01:46:17 +0200

    Remove unused parameters

--- src/main/java/org/bih/eos/controller/VisitController.java
+++ src/main/java/org/bih/eos/controller/VisitController.java
@@ -1,11 +1,8 @@
 package org.bih.eos.controller;
 
 import org.bih.eos.controller.dao.Ehrs;
-import org.bih.eos.jpabase.service.EHRToPersonService;
-import org.bih.eos.services.ConverterService;
 import org.bih.eos.services.VisitEndpointService;
 import org.bih.eos.services.VisitService;
-import org.ehrbase.client.openehrclient.OpenEhrClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,16 +17,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 @RequestMapping(path = "/visit")
 public class VisitController {
-    private final ConverterService converterService;
-    private final EHRToPersonService ehrToPersonService;
     private final VisitEndpointService visitEndpointService;
-    private final OpenEhrClient openEhrClient;
 
-    public VisitController(ConverterService conversionService, EHRToPersonService ehrToPersonService, VisitEndpointService visitEndpointService, OpenEhrClient openEhrClient) {
-        this.converterService = conversionService;
-        this.ehrToPersonService = ehrToPersonService;
+    public VisitController(VisitEndpointService visitEndpointService) {
         this.visitEndpointService = visitEndpointService;
-        this.openEhrClient = openEhrClient;
     }
 
     @PostMapping(
@@ -39,7 +30,7 @@ public class VisitController {
             @RequestParam(required = false) String templateid,
             @RequestParam(required = false) String visitsource
     		) {
-        return new ResponseEntity<>(new VisitService(ehrToPersonService,visitEndpointService, openEhrClient, converterService,aql).generateAllVisits(), HttpStatus.OK);
+        return new ResponseEntity<>(new VisitService(visitEndpointService, aql).generateAllVisits(), HttpStatus.OK);
     }
 
     @PostMapping(
@@ -49,7 +40,7 @@ public class VisitController {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             Ehrs ehrs = objectMapper.readValue(json, Ehrs.class);
-            return new ResponseEntity<>(new VisitService(ehrToPersonService,visitEndpointService, openEhrClient, converterService).generateSpecificVisits(ehrs), HttpStatus.OK);
+            return new ResponseEntity<>(new VisitService(visitEndpointService).generateSpecificVisits(ehrs), HttpStatus.OK);
         } catch (JsonProcessingException e) {
             return new ResponseEntity<>("{ \"message\" : \"Json malformed\" }", HttpStatus.BAD_REQUEST);
         }
--- src/main/java/org/bih/eos/services/VisitService.java
+++ src/main/java/org/bih/eos/services/VisitService.java
@@ -5,33 +5,22 @@ import java.util.List;
 
 import org.bih.eos.controller.dao.Ehrs;
 import org.bih.eos.jpabase.entity.PersonVisit;
-import org.bih.eos.jpabase.service.EHRToPersonService;
 import org.bih.eos.services.dao.ConversionResponseVisit;
-import org.ehrbase.client.openehrclient.OpenEhrClient;
 import org.slf4j.LoggerFactory;
 
 
 public class VisitService {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(VisitService.class);
-    private final EHRToPersonService ehrToPersonService;
     private final VisitEndpointService visitEndpointService;
-    private final OpenEhrClient openEhrClient;
-    private final ConverterService converterService;
     private String aql;
     int ehrBatchAmount = 40;
 
-    public VisitService(EHRToPersonService ehrToPersonService, VisitEndpointService visitEndpointService, OpenEhrClient openEhrClient, ConverterService converterService) {
-    	this.ehrToPersonService = ehrToPersonService;
+    public VisitService(VisitEndpointService visitEndpointService) {
     	this.visitEndpointService = visitEndpointService;
-    	this.openEhrClient = openEhrClient;
-    	this.converterService = converterService;
     }
 
-    public VisitService(EHRToPersonService ehrToPersonService, VisitEndpointService visitEndpointService, OpenEhrClient openEhrClient, ConverterService converterService, String aql) {
-    	this.ehrToPersonService = ehrToPersonService;
+    public VisitService(VisitEndpointService visitEndpointService, String aql) {
     	this.visitEndpointService = visitEndpointService;
-    	this.openEhrClient = openEhrClient;
-    	this.converterService = converterService;
 		this.aql = aql;
 
 	}
