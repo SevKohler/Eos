e3e2b8023c3c134ccca625d615f8d8249b01b41a
yampeku <yampeku@gmail.com>
2025-07-14 20:59:49 +0200

    Fixed VisitConverterProperties
    Added try catch
    Added error log

--- src/main/java/org/bih/eos/config/VisitConverterProperties.java
+++ src/main/java/org/bih/eos/config/VisitConverterProperties.java
@@ -1,13 +1,17 @@
 package org.bih.eos.config;
 
+import org.ehrbase.aql.parser.AqlParseException;
 import org.ehrbase.aql.parser.AqlToDtoParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "eos.visit-conversion")
 public class VisitConverterProperties {
-	
+
+	private static final Logger LOG = LoggerFactory.getLogger(VisitConverterProperties.class);
 	private String aql;
 	private String templateid;
 	private String visitsource;
@@ -18,8 +22,13 @@ public class VisitConverterProperties {
 
 	public void setAql(String aql) {
 		//validate AQL
-		new AqlToDtoParser().parse(aql);
-		this.aql = aql;
+		try {
+			new AqlToDtoParser().parse(aql);
+			this.aql = aql;
+		} catch (AqlParseException e) {
+			LOG.error("Visit AQL could not be parsed: {}",e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	public String getTemplateid() {
